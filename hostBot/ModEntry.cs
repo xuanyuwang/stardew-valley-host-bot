b6373a18c0c699c3f4864d3f534b2742995b8050
Xuanyu Wang <xuanyu.wang@outlook.com>
2025-03-30 16:47:08 -0400

    Allow run remote messages as commands

--- hostBot/ModEntry.cs
+++ hostBot/ModEntry.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Runtime.Intrinsics.X86;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -12,12 +14,13 @@ namespace hostBot
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        private bool IsHostBotMode;
-        private bool IsAutoSleep;
-        private int AutoSleepTime = 2200;
+        private bool _isHostBotMode;
+        private bool _isAutoSleep;
+        private int _autoSleepTime = 2200;
+        private string? _lastChatMessage;
 
         // game loop status
-        private bool DayEnding;
+        private bool _dayEnding;
 
         /*********
          ** Public methods
@@ -32,38 +35,24 @@ namespace hostBot
             helper.Events.GameLoop.SaveCreated += (sender, args) => { Monitor.Log("Save Created", LogLevel.Info); };
             helper.Events.GameLoop.Saving += (sender, args) => { Monitor.Log("Saving", LogLevel.Info); };
             helper.Events.GameLoop.Saved += (sender, args) => { Monitor.Log("Saved", LogLevel.Info); };
-            helper.Events.GameLoop.SaveLoaded += (sender, args) =>
-            {
-                Monitor.Log("Save Loaded", LogLevel.Info);
-                Game1.chatBox.chatBox.OnEnterPressed += (sender) =>
-                {
-                    Monitor.Log("sender's text: " + sender.Text, LogLevel.Info);
-                    if (sender is ChatTextBox chatTextBox)
-                    {
-                        if (chatTextBox.finalText.Count > 0)
-                        {
-                            Monitor.Log("final text: " + ChatMessage.makeMessagePlaintext(chatTextBox.finalText, false),
-                                LogLevel.Info);
-                        }
-                    }
-                };
-            };
+            helper.Events.GameLoop.SaveLoaded += (sender, args) => { Monitor.Log("Save Loaded", LogLevel.Info); };
 
             // set / unset DayEnding
             helper.Events.GameLoop.DayStarted += (sender, args) =>
             {
                 Monitor.Log("Day Started", LogLevel.Info);
-                this.DayEnding = false;
+                this._dayEnding = false;
             };
             helper.Events.GameLoop.DayEnding += (sender, args) =>
             {
                 Monitor.Log("Day Ending", LogLevel.Info);
-                this.DayEnding = true;
+                this._dayEnding = true;
             };
             helper.Events.GameLoop.OneSecondUpdateTicking += (sender, args) =>
             {
                 CloseShippingMenu();
                 CloseAnyBlockingMenus();
+                TryRunChatMessage();
             };
             helper.Events.GameLoop.TimeChanged += (sender, args) => { AutoGoToBed(); };
 
@@ -75,7 +64,7 @@ namespace hostBot
                 {
                     PauseWorld(false);
                     this.HostBotCommands("", new string[] { "on" });
-                    this.HostBotCommands("", new string[] { "sleep", AutoSleepTime.ToString() });
+                    this.HostBotCommands("", new string[] { "sleep", _autoSleepTime.ToString() });
                 }
             };
 
@@ -105,9 +94,9 @@ namespace hostBot
         private void AutoGoToBed()
         {
             var currentTime = Game1.timeOfDay;
-            if (this.IsAutoSleep)
+            if (this._isAutoSleep)
             {
-                if (currentTime >= AutoSleepTime)
+                if (currentTime >= _autoSleepTime)
                 {
                     GoToBed(new string[] { });
                 }
@@ -119,7 +108,7 @@ namespace hostBot
         /// </summary>
         private void CloseShippingMenu()
         {
-            if (this.IsAutoSleep && this.DayEnding)
+            if (this._isAutoSleep && this._dayEnding)
             {
                 if (Game1.activeClickableMenu is ShippingMenu)
                 {
@@ -145,7 +134,7 @@ namespace hostBot
         /// </summary>
         private void CloseAnyBlockingMenus()
         {
-            if (this.IsAutoSleep && this.DayEnding)
+            if (this._isAutoSleep && this._dayEnding)
             {
                 if (Game1.activeClickableMenu != null && Game1.activeClickableMenu.IsActive() &&
                     Game1.activeClickableMenu.readyToClose() &&
@@ -209,15 +198,15 @@ namespace hostBot
             {
                 case "no":
                     Game1.chatBox.addMessage("the host bot will NOT auto sleep", Color.Yellow);
-                    IsAutoSleep = false;
+                    _isAutoSleep = false;
                     break;
                 default:
                     var validTime = arg.Length == 4 && arg.All(char.IsDigit);
                     if (validTime)
                     {
                         Game1.chatBox.addMessage($"the host bot will auto sleep at {arg}", Color.Yellow);
-                        AutoSleepTime = int.Parse(arg);
-                        IsAutoSleep = true;
+                        _autoSleepTime = int.Parse(arg);
+                        _isAutoSleep = true;
                     }
 
                     break;
@@ -267,17 +256,46 @@ namespace hostBot
         private void ToggleHostBotMode(bool targetStatus)
         {
             if (!Context.IsWorldReady) return;
-            if (IsHostBotMode == targetStatus) return;
+            if (_isHostBotMode == targetStatus) return;
 
-            IsHostBotMode = targetStatus;
+            _isHostBotMode = targetStatus;
             var status = targetStatus ? "on" : "off";
             var message = $"The host bot mode is now {status}.";
             this.Monitor.Log(message, LogLevel.Info);
             Game1.chatBox.addMessage(message, Color.Yellow);
         }
 
-        private void parseChatMessage(string command, string[] args)
+        private void TryRunChatMessage()
         {
+            if(!Context.IsWorldReady) return;
+            if(Game1.chatBox.messages.Count == 0) return;
+
+            var lastMessage = Game1.chatBox.messages.Last();
+            var plainText = ChatMessage.makeMessagePlaintext(lastMessage.message, false);
+            if(plainText == _lastChatMessage) return;
+
+            _lastChatMessage = plainText;
+            var (command, args) = ParseChatMessage(plainText);
+            if (command != null && args != null)
+            {
+                HostBotCommands(command, args);
+            }
+        }
+
+        private (string?, string[]?) ParseChatMessage(string chatMessage)
+        {
+            Monitor.Log(chatMessage, LogLevel.Info);
+            var trimmedMessage = chatMessage.Trim();
+            if(trimmedMessage.Length == 0) return (null, null);
+            Monitor.Log($"trimmed message {chatMessage}", LogLevel.Info);
+            
+            var splitMessages = trimmedMessage.Split("!!");
+            if (splitMessages.Length == 1) return (null, null);
+            
+            var command = splitMessages[1];
+            Monitor.Log($"command from message {command}", LogLevel.Info);
+            var commandParts = command.Split(' ');
+            return (commandParts[0], commandParts.Skip(1).ToArray());
         }
     }
 }
